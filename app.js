0301567ab59d6d4b67722def644406c5f2339735
Sanjay Sharma~ <sanjay.delhi18@gmail.com>
2025-12-14 21:12:22 +0530

    Final Version of App

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to K21 Academy Project 2025'));
+app.get('/', (req, res) => res.send('Production Final Version Application 2025!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
